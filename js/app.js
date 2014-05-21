ea7b1eb364ea44733a13405f7b4c04871f3c5ed8
Victoria Solomon <vasolomon@gmail.com>
2014-05-20 21:10:27 -0700

    Adding jQuery

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function(){
+	$('.ryu-still').hide();
+	$('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+	$('.ryu-ready').hide();
+	$('.ryu-still').show();
+  })
+   .mousedown(function() {
+    playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show().animate({'left': '750px'}, 500, function () {
+	$(this).hide();
+	$(this).css('left', '450px');
+    });
+  })
+  .mouseup(function() {
+    $('ryu-throwing').hide();
+    $('ryu-ready').show();
+    // ryu goes back to his ready position
+  });
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
