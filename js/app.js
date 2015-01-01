8f7c169ac7dc1bc6e7035acac1a635076815be87
Victoria Solomon <vasolomon@gmail.com>
2014-12-31 16:50:48 -0800

    Completed!

--- js/app.js
+++ js/app.js
@@ -1,25 +1,39 @@
 $(document).ready(function() {
   $('.ryu').mouseenter(function(){
-	$('.ryu-still').hide();
-	$('.ryu-ready').show();
+	 $('.ryu-still').hide();
+	 $('.ryu-ready').show();
   })
   .mouseleave(function() {
-	$('.ryu-ready').hide();
+	$('.ryu-throwing').hide();
+  $('.ryu-ready').hide();
 	$('.ryu-still').show();
   })
-   .mousedown(function() {
+  .mousedown(function() {
     playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show().animate({'left': '750px'}, 500, function () {
-	$(this).hide();
-	$(this).css('left', '450px');
+	     $(this).hide();
+	     $(this).css('left', '450px');
     });
   })
   .mouseup(function() {
-    $('ryu-throwing').hide();
-    $('ryu-ready').show();
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
     // ryu goes back to his ready position
+  });
+  $(document).keydown(function() {
+    if (event.which == 88) {
+    $('.ryu-still').hide();
+    $('.ryu-ready').hide();
+    $('.ryu-cool').show();
+  }
+  });
+    $(document).keyup(function() {
+      if (event.which == 88) {
+    $('.ryu-cool').hide();
+    $('.ryu-ready').hide();
+    $('.ryu-ready').show(); }
   });
 });
 
